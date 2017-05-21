6b2c15d1b6c4db7ba90160dba8fadc544431e163
No1ChalresWu <No1CharlesWu@163.com>
2017-05-21 21:25:01 +0800

    写了半天的threadpool，一堆坑。明天再弄。

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # import config
 from config import Config
 import database
+import schedule
 
 if __name__ == '__main__':
     config = Config()
     db = database.DataBase(config.db, config.task)
-
+    print(type(db))
+    schedule = schedule.Schedule(config.task, db)
     # print(config.configs)
     # print('task', config.task)
--- schedule.py
+++ schedule.py
@@ -4,8 +4,30 @@ import threadpool
 class Schedule(object):
     def __init__(self, l_task, db):
         self.db = db
+        task_pool = threadpool.ThreadPool(1)
 
-        pass
+        request_list = []
+        for task in l_task:
+            t = ((task,self.db),dict())
+            print(type(t))
+            request_list.append(threadpool.makeRequests(self.ThreadFun,([task,self.db],{}), callback=self.Callback()))
+
+        for request in request_list:
+            task_pool.putRequest(*request)
+        task_pool.wait()
+
 
     def add_task(self, task):
         pass
+
+    def Callback(self):
+        print('Callback')
+
+    def ThreadFun(self, *arg1,**arg2):
+        print('ThreadFun', arg1, arg2)
+        print('arg1',type(arg1), arg1)
+        print('arg2',type(arg2), arg2)
+
+if __name__ == '__main__':
+    a = Schedule(['1','2'], {'db':'root'})
+
